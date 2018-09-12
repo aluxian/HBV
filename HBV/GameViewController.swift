3767816f97779e6c1dc3fb1933a12b835eeb06e1
Alexandru Rosianu <me@aluxian.com>
2018-09-12 12:30:13 +0200

    Check in everything

--- HBV/GameViewController.swift
+++ HBV/GameViewController.swift
@@ -1,8 +1,8 @@
 //
 //  GameViewController.swift
-//  HBV
+//  Pongicky
 //
-//  Created by Alexandru Rosianu on 06/09/2018.
+//  Created by Alexandru Rosianu on 03/09/2018.
 //  Copyright © 2018 AR. All rights reserved.
 //
 
@@ -11,45 +11,42 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
             view.ignoresSiblingOrder = true
+            //            view.showsFPS = true
+            //            view.showsNodeCount = true
+            //            view.showsPhysics = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .resizeFill
+            
+            view.presentScene(scene)
         }
     }
-
+    
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    override func prefersHomeIndicatorAutoHidden() -> Bool {
+        return true
+    }
+    
 }
